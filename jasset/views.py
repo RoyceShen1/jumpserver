ddc60f844520b4bcf521d7456431a82da190635c
zhangwenhao <zhangwenhao@fangdd.com>
2016-12-09 13:34:42 +0800

    加入按ip排序的机制

--- jasset/views.py
+++ jasset/views.py
@@ -345,8 +345,9 @@ def asset_list(request):
             file_name = s[1]
         smg = u'excel文件已生成，请点击下载!'
         return my_render('jasset/asset_excel_download.html', locals(), request)
-    # 去重
+    # 去重,按ip排序
     asset_find = asset_find.distinct()
+    asset_find = sorted(asset_find, key=lambda ip: long(''.join(["%02X" % long(i) for i in ip.ip.split('.')]), 16))
 
     assets_list, p, assets, page_range, current_page, show_first, show_end = pages(asset_find, request)
     if user_perm != 0:
