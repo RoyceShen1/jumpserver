1f89ed51f44dd1f262e3bd95013407d1f2db890e
zhangwenhao <zhangwenhao@fangdd.com>
2016-12-12 13:36:44 +0800

    修改生成excel中的排序,按ip进行排序

--- jasset/views.py
+++ jasset/views.py
@@ -340,6 +340,7 @@ def asset_list(request):
                 asset = get_object(Asset, id=asset_id)
                 if asset:
                     asset_find.append(asset)
+        asset_find = sorted(asset_find, key=lambda ip: long(''.join(["%02X" % long(i) for i in ip.ip.split('.')]), 16))
         s = write_excel(asset_find)
         if s[0]:
             file_name = s[1]
