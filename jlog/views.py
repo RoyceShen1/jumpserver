aca025d3f6f8b5dc61ef1d91c3bdcb61ea9509e1
zhangwenhao <zhangwenhao@fangdd.com>
2017-04-10 18:03:32 +0800

    bug fix

--- jlog/views.py
+++ jlog/views.py
@@ -403,15 +403,15 @@ def log_report(request):
         users = User.objects.all().order_by('-week_times')
     if month == '1':
         users = User.objects.all().order_by('month_times')
-    else month == '0':
+    elif month == '0':
         users = User.objects.all().order_by('-month_times')
     if quarter == '1':
         users = User.objects.all().order_by('quarter_times')
-    else quarter == '0':
+    elif quarter == '0':
         users = User.objects.all().order_by('-quarter_times')
     if year == '1':
         users = User.objects.all().order_by('year_times')
-    else year == '0':
+    elif year == '0':
         users = User.objects.all().order_by('-year_times')
     
     if not week and not month and not quarter and not year:
