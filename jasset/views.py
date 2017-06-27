6884e7c68667d3c44e3a1916a24a58eef01e0524
zhangwenhao <zhangwenhao@fangdd.com>
2017-06-27 15:32:49 +0800

    add get method filter

--- jasset/views.py
+++ jasset/views.py
@@ -852,7 +852,7 @@ def xenserver_api(request):
     return HttpResponse(json.dumps(xen_info_list), content_type = "application/json")
 
 def group_info(request):
-    key = request.GET.get(q,'')
+    key = request.GET.get('q','')
     groups = AssetGroup.objects.filter(name__contains=key)
     group_hosts = {}
     for group in groups:
