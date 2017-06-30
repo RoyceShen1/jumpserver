cae5ceea48cea3c4d4b1142a7a25558c3b5580a5
zhangwenhao <zhangwenhao@fangdd.com>
2017-06-30 17:32:03 +0800

    add periodic task for root check

--- jumpserver/celeryconfig.py
+++ jumpserver/celeryconfig.py
@@ -2,6 +2,12 @@
 from datetime import timedelta
 from celery.schedules import crontab
 
+import os,sys
+sys.path.append('../')
+os.environ["DJANGO_SETTINGS_MODULE"] = 'jumpserver.settings'
+from juser.models import User
+admin = User.objects.get(username='admin')
+
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 # CELERY_TASK_SERIALIZER = 'json'
@@ -15,5 +21,10 @@ CELERYBEAT_SCHEDULE = {
     'add-each-minute':{
         'task': 'task_start_ping',
         'schedule': timedelta(seconds=3600),
+    }
+    'root-check-each-day':{
+    	'task': 'task_root_check',
+    	'schedule': timedelta(seconds=86400),
+    	'args': (admin),
     }
 }
